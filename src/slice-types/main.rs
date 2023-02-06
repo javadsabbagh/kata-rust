40780ad900609bc64ace4196337676300ab6bc70
javadsabbagh <javad.sabbaq@gmail.com>
2023-02-07 00:19:37 +0330

    number slice sample

--- src/slice-types/main.rs
+++ src/slice-types/main.rs
@@ -2,12 +2,16 @@
 
 fn main() {
     let mut s = String::from("Mr Barzin");
+    let word = first_word(&s);  // immutable borrowing
+    println!("The first word is: {}", word); // immutable borrowing
+    s.clear();  // mutable borrowing. Note: it must be after immutable borrowing, unless it'll get compile eeor
+    println!("{}", s);
 
-    let word = first_word(&s);
 
-    println!("The first word is: {}", word);
-
-    s.clear()
+    // array slice sample
+    let a = [1, 2, 3, 4, 5];
+    let slice = &a[1..=3];
+    assert_eq!(slice, &[2, 3, 4])
 }
 
 fn first_word(st: &String) -> &str {
