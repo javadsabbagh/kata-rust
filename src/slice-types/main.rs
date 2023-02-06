96ebf401c28ec658c05ec1dd3b4426d1fb0cc4e2
javadsabbagh <javad.sabbaq@gmail.com>
2023-02-07 00:10:26 +0330

    string slice sample

--- src/slice-types/main.rs
+++ src/slice-types/main.rs
@@ -1,5 +1,23 @@
 
 
 fn main() {
+    let mut s = String::from("Mr Barzin");
 
+    let word = first_word(&s);
+
+    println!("The first word is: {}", word);
+
+    s.clear()
+}
+
+fn first_word(st: &String) -> &str {
+    let bytes = st.as_bytes();
+
+    for (i, &item ) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &st[0..i];
+        }
+    }
+
+    return &st[..]
 }
