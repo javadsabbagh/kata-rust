8821aacd0be009be6e88e67f89f45e8a8fc8dfb4
javadsabbagh <javad.sabbaq@gmail.com>
2023-02-08 22:05:52 +0330

    Add other samples

--- src/slice-types/main.rs
+++ src/slice-types/main.rs
@@ -1,12 +1,10 @@
 
-
 fn main() {
     let mut s = String::from("Mr Barzin");
     let word = first_word(&s);  // immutable borrowing
     println!("The first word is: {}", word); // immutable borrowing
     s.clear();  // mutable borrowing. Note: it must be after immutable borrowing, unless it'll get compile eeor
     println!("{}", s);
-
 
     // array slice sample
     let a = [1, 2, 3, 4, 5];
