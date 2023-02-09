fad81e138340dab94eab294efea5fb78e3f8f137
javadsabbagh <javad.sabbaq@gmail.com>
2023-02-10 00:06:15 +0330

    environment variables

--- src/env-variables/main.rs
+++ src/env-variables/main.rs
@@ -2,10 +2,15 @@
 
 // Note: there are many options available with std::env module. Please look at documents.
 fn main() {
-    println!("{}", env!("SHELL"));
 
+    println!("{:?}", std::env::current_exe());  // returns io::Result<PathBuf>
 
+    // iterate through all environment variables, each item is a tuple of key-value.
     for item in std::env::vars() {
         println!("{}={}", item.0, item.1)
     }
+
+    // using vec! macro, collects enviroment variable at compile time
+    println!("{}", env!("SHELL"));            // compile error if vaiable does not exist.
+    println!("{:?}", option_env!("SHELL"));   // returns Option and no compile error if variable does not exists.
 }
