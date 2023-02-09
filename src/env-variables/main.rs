b41f11824e5d1c38eca728fb0a572e2284aa8f4d
javadsabbagh <javad.sabbaq@gmail.com>
2023-02-09 23:59:21 +0330

    environment variables

--- src/env-variables/main.rs
+++ src/env-variables/main.rs
@@ -1,4 +1,11 @@
 
-fn main() {
 
+// Note: there are many options available with std::env module. Please look at documents.
+fn main() {
+    println!("{}", env!("SHELL"));
+
+
+    for item in std::env::vars() {
+        println!("{}={}", item.0, item.1)
+    }
 }
