2639c61dffa536b1f89aa0dc4a5d61007390c748
javadsabbagh <javad.sabbaq@gmail.com>
2023-02-24 20:43:23 +0330

    Playing with sets

--- rust-lang/src/sets/main.rs
+++ rust-lang/src/sets/main.rs
@@ -1,5 +1,16 @@
-
+use std::collections::{BTreeSet, HashSet};
+use std::hash::Hash;
+use std::ptr::hash;
 
 fn main() {
-    
+
+    // A set has two varieties: BTreeSet and HashSet
+    let mut hash_set = HashSet::new();
+    let another_hash_Set: HashSet<&str> = HashSet::new();
+    let mut tree_set = BTreeSet::new();
+
+    hash_set.insert("ABC");
+    tree_set.insert("EFG");
+
+    hash_set.intersection(&another_hash_Set);
 }
