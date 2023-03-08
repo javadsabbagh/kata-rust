972c5a8dd4830879b4d45d1abb52af44f2f2e1ce
javadsabbagh <javad.sabbaq@gmail.com>
2023-03-08 23:42:51 +0330

    Run actix-example

--- samples/actix-example/src/main.rs
+++ samples/actix-example/src/main.rs
@@ -1,6 +1,6 @@
 #![allow(unused_imports)]
 
-use actix_web::{App, get, HttpResponse, HttpServer, Responder, web};
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -18,18 +18,14 @@ struct ServerInfo {
 
 #[derive(Debug, Serialize, Deserialize)]
 struct ResourceInfo {
-    cpu: u8,
-    /* number of cpu cores */
-    memory: u16,
-    /* memory size in megabytes */
-    disk: u64,
-    /* disk size in megaytes*/
+    cpu: u8,    /* number of cpu cores */
+    memory: u16, /* memory size in megabytes */
+    disk: u64  /* disk size in megabytes */
 }
 
-
 #[get("/get-all")]
-async fn get_all() -> impl Responder {
-    let servesList = ServerList {
+async fn get_all() -> ServerList {
+    ServerList {
         serverInfos: vec![ServerInfo {
             id: String::from("a7957610-bc59-11ed-8f6c-88a4c2e3226d"),
             ip: String::from("185.121.123.90"),
@@ -38,19 +34,13 @@ async fn get_all() -> impl Responder {
                 memory: 3_000_u16,
                 disk: 100_000_u64,
             },
-        }, ]
-    };
-
-    HttpResponse::Ok().body(servesList)
+        }],
+    }
 }
-
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(get_all)
-    })
+    HttpServer::new(|| App::new().app_data({}).service(get_all))
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
