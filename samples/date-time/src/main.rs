51e986150d067d9da1f960d7dadc917c13e6d9dd
javadsabbagh <javad.sabbaq@gmail.com>
2023-03-11 23:55:34 +0330

    Add datetime sample

--- samples/date-time/src/main.rs
+++ samples/date-time/src/main.rs
@@ -1,3 +1,14 @@
+
+use chrono::Utc;
+use chrono::Local;
+use chrono::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    let utc_time = Utc::now();
+    let local_time = Local::now();
+
+    println!("UTC time is: {}", utc_time);
+    println!("Local time is: {}", local_time);
+
+    Duration::minutes(1);
 }
