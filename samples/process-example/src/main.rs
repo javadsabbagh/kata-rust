fb6b194b825d28dd319271ad930695547f90a4ce
javadsabbagh <javad.sabbaq@gmail.com>
2023-03-16 23:43:53 +0330

    Complete process example

--- samples/process-example/src/main.rs
+++ samples/process-example/src/main.rs
@@ -1,3 +1,18 @@
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    let output = Command::new("ps")
+        .arg("aux")
+        .output()
+        .unwrap_or_else(|e| panic!("failed to execute process: {}", e));
+
+    if output.status.success() {
+        let s = String::from_utf8_lossy(&output.stdout);
+
+        print!("ps aux succeeded and stdout was:\n{}", s);
+    } else {
+        let s = String::from_utf8_lossy(&output.stderr);
+
+        print!("ps aux failed and stderr was:\n{}", s);
+    }
 }
