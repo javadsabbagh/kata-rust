4f934b12d1aa1819cdc954ecf0b119d6e6ba7e7f
javadsabbagh <javad.sabbaq@gmail.com>
2023-03-20 23:50:19 +0330

    log-example: env_logger

--- samples/log-example/src/main.rs
+++ samples/log-example/src/main.rs
@@ -1,3 +1,9 @@
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+
+    // To see log messages lower than error use `RUST_LOG=info cargo run`
+    log::info!("Just an info message...");
+    log::warn!("Caution: take this seriously...");
+    log::error!("Error occurred...");
 }
