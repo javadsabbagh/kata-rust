2c4337fccb9cf698b367118bdac4b6a20f289dd5
javadsabbagh <javad.sabbaq@gmail.com>
2023-03-21 23:57:04 +0330

    Type conversion example

--- samples/log-example/src/main.rs
+++ samples/log-example/src/main.rs
@@ -1,8 +1,8 @@
 
 fn main() {
-    // env_logger::init();
+     env_logger::init();
 
-    log4rs::init_file("log4rs.yml", Default::default()).unwrap();
+    // log4rs::init_file("log4rs.yml", Default::default()).unwrap();
 
     // To see log messages lower than error use `RUST_LOG=info cargo run`
     log::info!("Just an info message...");
