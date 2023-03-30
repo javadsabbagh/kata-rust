47ab24801595affa76eb1e4c19cc0a9f1ac6c09f
javadsabbagh <javad.sabbaq@gmail.com>
2023-03-31 00:11:13 +0330

    Update code

--- samples/log-example/src/main.rs
+++ samples/log-example/src/main.rs
@@ -1,8 +1,13 @@
 
-fn main() {
-     env_logger::init();
 
-    // log4rs::init_file("log4rs.yml", Default::default()).unwrap();
+fn main() {
+
+    // default log level for env_logger is error, unless it is set in RUST_LOG environment variable.
+    // e.g. RUST_LOG=info cargo run application
+    std::env::set_var("RUST_LOG", "info");
+    env_logger::init();
+
+    //log4rs::init_file("log4rs.yaml", Default::default()).unwrap();
 
     // To see log messages lower than error use `RUST_LOG=info cargo run`
     log::info!("Just an info message...");
