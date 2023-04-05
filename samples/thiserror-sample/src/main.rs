6f2d4584191bc70dd0b3c585fd3f92197b8d16df
javadsabbagh <javad.sabbaq@gmail.com>
2023-04-06 00:00:48 +0330

    play around with thiserror

--- samples/thiserror-sample/src/main.rs
+++ samples/thiserror-sample/src/main.rs
@@ -1,3 +1,19 @@
+use crate::ServiceCallError::ApplicationError;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum ServiceCallError {
+    #[error("Remote service call returned with error (rejected the request)")]
+    ServiceError(),
+    #[error("There was a connection error, e.g. timeout, token error, gateway error, etc.")]
+    ConnectionError,
+    #[error("There was an error (bug) in our application. Cause: `{0}`")]
+    ApplicationError(String),
+}
+
 fn main() {
     println!("Hello, world!");
+
+    let result = Err::<(), ServiceCallError>(ApplicationError("Division by zero".to_string()));
+    println!("{:?}", result)
 }
