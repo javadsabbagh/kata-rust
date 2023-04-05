35bd8c85d186beb642d6dbe66ccf1817e281ac0a
javadsabbagh <javad.sabbaq@gmail.com>
2023-04-06 00:14:49 +0330

    play around with thiserror crate

--- samples/thiserror-sample/src/main.rs
+++ samples/thiserror-sample/src/main.rs
@@ -1,5 +1,6 @@
-use crate::ServiceCallError::ApplicationError;
 use thiserror::Error;
+
+use crate::ServiceCallError::{ApplicationError, ConnectionError};
 
 #[derive(Error, Debug)]
 pub enum ServiceCallError {
@@ -11,9 +12,26 @@ pub enum ServiceCallError {
     ApplicationError(String),
 }
 
+#[derive(Error, Debug)]
+pub enum TokenError {
+    #[error("Invalid token provided")]
+    InvalidToken,
+    #[error("This token is expired, code: `{0}`")]
+    ExpiredToken(i32),
+    #[error("This token doesn't have access to the resource, code: `{0}`")]
+    UnAuthorizedAccess(i32),
+}
+
+
 fn main() {
     println!("Hello, world!");
 
     let result = Err::<(), ServiceCallError>(ApplicationError("Division by zero".to_string()));
-    println!("{:?}", result)
+    eprintln!("{:?}", result);
+
+    match result {
+        Err(ApplicationError(_)) => println!("Got an application error"),
+        Ok(()) => println!("Success"),
+        _ => println!("Other error types")
+    }
 }
