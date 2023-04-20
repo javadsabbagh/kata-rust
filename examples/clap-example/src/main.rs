3fb5c43f1faee9d9f2165f326da5a3f482caa198
javadsabbagh <javad.sabbaq@gmail.com>
2023-04-20 20:29:21 +0330

    Add LetsGetRusty code samples

--- examples/clap-example/src/main.rs
+++ examples/clap-example/src/main.rs
@@ -8,5 +8,21 @@ struct Options1 {
 
 }
 
+use std::process;
+use clap::{Arg, ArgMatches, App, SubCommand};
+
 fn main() {
+    let matches = App::new("24daysofrust")
+        .version("0.1")
+        .author("Zbigniew Siciarz")
+        .about("learn you some Rust!")
+        .arg(Arg::with_name("verbose")
+            .short("v")
+            .multiple(true)
+            .help("verbosity level"))
+        .get_matches();
+    if let Err(e) = run(matches) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
---
+++ lets-get-rusty/README.md
@@ -0,0 +1,3 @@
+# The Rust Developer Bootcamp
+
+> Note: This Repository is copied from 'https://github.com/letsgetrusty/bootcamp' repository.
